d3af6ac9b95895916902c31dc15106251abb7e5a
ataghav <ataghav@gmail.com>
2018-11-18 22:05:03 +0330

    New model amd message added

---
+++ Server/src/main/java/com/team/project/gameserver/models/MessageType.java
@@ -0,0 +1,11 @@
+package com.team.project.gameserver.models;
+
+public enum MessageType {
+    PLAYER_JOINED,
+    PLAYER_LEFT,
+    PLAYER_READY,
+    PLAYER_SELECTED,
+    QUESTION_SUBMITED,
+    ANSWER_SUBMITED,
+    SCORE_ADDED
+}
