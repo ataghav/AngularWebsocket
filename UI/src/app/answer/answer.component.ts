ed0221045f77e6f1d85eb29305374f0a883ae085
Abdi <ataghav@hotmail.com>
2018-11-24 13:32:34 +0330

    Redundant comments removed

--- UI/src/app/answer/answer.component.ts
+++ UI/src/app/answer/answer.component.ts
@@ -23,30 +23,20 @@ export class AnswerComponent implements OnDestroy {
   selectedOption: number;
   subscription: Subscription;
 
-  justReceivedQuestion: ReceivedQuestion;
+  justReceivedQuestion: ReceivedQuestion={
+    text:'',
+    options:[]
+  };
 
   constructor(private interCommService: InterCommService) {
     this.subscription = interCommService.questionSubmited$.subscribe(
       message => {
-        // this.message = message;
-        // console.log('I am player and I have Got your message!!!!');
-        // console.log(message);
         var parsedMessage = JSON.parse(message);
         console.log(parsedMessage.options);
         this.justReceivedQuestion.text = parsedMessage.question;
         this.justReceivedQuestion.options = parsedMessage.options;
       });
   }
-
-  // TODO: load question dynamically
-  justReceivedQuestion = {
-    text: 'Is this a sample questoin text?',
-    options: [
-      { text: 'option1', index: 1},
-      { text: 'option2', index: 2},
-      { text: 'option3', index: 3},
-    ]
-  };
 
   selectionChanged(event: any) {
     this.selectedOption = event.target.value;
