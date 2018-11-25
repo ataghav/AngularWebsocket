0ddfef9d9c3465384e3a6c8ff9c53e554085fe32
ataghav <ataghav@gmail.com>
2018-11-25 23:26:02 +0330

    Security  added to the serversome refactoring on client

---
+++ Server/src/main/java/com/team/project/gameserver/configuration/BasicAuthConfiguration.java
@@ -0,0 +1,4 @@
+package com.team.project.gameserver.configuration;
+
+public class BasicAuthConfiguration {
+}
---
+++ Server/src/main/java/com/team/project/gameserver/controllers/UserController.java
@@ -0,0 +1,4 @@
+package com.team.project.gameserver.controllers;
+
+public class UserController {
+}
