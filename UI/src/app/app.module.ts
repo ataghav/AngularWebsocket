01ac079ab5cfbcc422313cd9d8caef3ba2a50037
Abdi <ataghav@hotmail.com>
2018-11-27 17:48:28 +0330

    Client, flex and rx-compat added

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { PlayersComponent } from './players/players.component';
@@ -25,7 +26,8 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule
+    FormsModule,
+    FlexLayoutModule
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [],
