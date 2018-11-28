73f4ff90155ea07901f13c2c4a7dab8742ab2683
Abdi <ataghav@hotmail.com>
2018-11-28 13:42:52 +0330

    Client, Conflicting libraries remoced

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
+// import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { PlayersComponent } from './players/players.component';
@@ -27,7 +27,7 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    FlexLayoutModule
+    // FlexLayoutModule
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [],
